b4cce57e9c39d8ec26a176c624c970100b67cf5e
thony-ui <anthonyhermanto1@gmail.com>
2025-04-27 16:29:15 +0800

    modifytest case

--- src/modules/mongo-test/test/mongo-test.test.ts
+++ src/modules/mongo-test/test/mongo-test.test.ts
@@ -1,6 +1,5 @@
 import request from "supertest";
 import { app, server } from "../../../test-app";
-import { Server } from "http";
 import dotenv from "dotenv";
 import { seedDatabase } from "../../../seed/seed";
 
@@ -19,12 +18,10 @@ afterAll(() => {
   server.close();
 });
 describe("MongoTest API", () => {
-  describe("GET /", () => {
-    it("should return 200 for the base route", async () => {
-      const res = await request(app).get("/");
+  it("should return 200 for the base route", async () => {
+    const res = await request(app).get("/");
 
-      expect(res.status).toBe(200);
-    });
+    expect(res.status).toBe(200);
   });
   it("should return 200 and an array of students", async () => {
     const res = await request(app).get("/mongo-test/students");
