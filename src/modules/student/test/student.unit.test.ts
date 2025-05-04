531ff6d4b04ea1d338b87fb9ee8382429d0568a5
thony-ui <anthonyhermanto1@gmail.com>
2025-05-04 15:29:56 +0800

    Add controller unit tests

--- src/modules/student/test/student.unit.test.ts
+++ src/modules/student/test/student.unit.test.ts
@@ -1,9 +1,11 @@
+import { StudentsController } from "../domain/student.controller";
 import { IStudentModel } from "../domain/student.model";
 import { StudentRepository } from "../domain/student.repository";
 import { StudentService } from "../domain/student.service";
 import { ObjectId } from "mongodb";
+import type { Request, Response, NextFunction } from "express";
 
-describe("Student Unit Tests", () => {
+describe("Student service unit tests", () => {
   let studentService: StudentService;
   let studentRepositoryMock: jest.Mocked<StudentRepository>;
 
@@ -27,5 +29,49 @@ describe("Student Unit Tests", () => {
 
     expect(students).toEqual(mockStudents);
     expect(studentRepositoryMock.getAllStudentsService).toHaveBeenCalled();
+  });
+});
+
+describe("Student controller unit tests", () => {
+  let studentController: StudentsController;
+  let studentServiceMock: jest.Mocked<StudentService>;
+  let request: Partial<Request>;
+  let response: Partial<Response>;
+  let nextFunction: NextFunction;
+
+  beforeEach(() => {
+    studentServiceMock = {
+      getAllStudentsService: jest.fn(),
+    } as unknown as jest.Mocked<StudentService>;
+    request = {
+      params: {},
+      body: {},
+      query: {},
+    };
+    response = {
+      json: jest.fn(),
+      status: jest.fn().mockReturnThis(),
+    };
+    nextFunction = jest.fn();
+    studentController = new StudentsController(studentServiceMock);
+  });
+
+  it("should fetch all students", async () => {
+    const mockStudents: IStudentModel[] = [
+      { _id: new ObjectId(), name: "Alice", age: 21 },
+      { _id: new ObjectId(), name: "Bob", age: 22 },
+    ];
+
+    studentServiceMock.getAllStudentsService.mockResolvedValue(mockStudents);
+
+    await studentController.getAllStudentsController(
+      request as Request,
+      response as Response,
+      nextFunction
+    );
+
+    expect(response.status).toHaveBeenCalledWith(200);
+    expect(studentServiceMock.getAllStudentsService).toHaveBeenCalled();
+    expect(response.json).toHaveBeenCalledWith(mockStudents);
   });
 });
