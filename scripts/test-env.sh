ab08722d538605f981c47278ce0f9605d9c0c5cd
thony-ui <anthonyhermanto1@gmail.com>
2025-05-04 12:57:57 +0800

    Added unit tests

--- scripts/test-env.sh
+++ scripts/test-env.sh
@@ -3,8 +3,11 @@
 echo "🐳 Starting Docker Compose..."
 docker compose -p "test" -f infra/docker/docker-compose.test.yml up --build -d
 
-echo "🧪 Running tests inside container..."
-docker exec node-app-test npm run test:only
+echo "🧪 Running end to end tests inside container..."
+docker exec node-app-test npm run test:e2e
+
+echo "🧪 Running unit tests inside container..."
+docker exec node-app-test npm run test:unit
 
 echo "🧹 Cleaning up Docker Compose..."
 docker compose -p "test" -f infra/docker/docker-compose.test.yml down -v
